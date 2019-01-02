75291501ebaf19f0d6f2036fe1d46c075edfdc53
System.rat <system.rodent@gmail.com>
2019-01-02 19:18:14 +0100

    CopyOperation changes
    - Preparing to move the local copy code to LocalCopy struct

--- src/copy_operation.rs
+++ src/copy_operation.rs
@@ -1,47 +1,27 @@
 /// A trait for copy operations done on targets
 /// TODO: Implement current copy operations using this trait
 pub trait CopyOperation {
-    /// The type of the path variables
-    type PathType;
     /// The type of error returned by the operations
-    type Error;
+    type Error: std::error::Error;
 
-    /// Copies an item from one place to another.
+    /// Performs the copy operation for the target.
     ///
-    /// #Parameters
+    /// # Parameters
+    /// - threads: the number of threads (<2 is single threaded)
     ///
-    /// -from: The path of the item
-    /// -to: The path of the destination
-    /// -check_timestamp: Whether to check if the file was changed
-    /// -num_threads: The number of threads to use (none if 1 or 0)
-    ///
-    /// #Returns
+    /// # Returns
     /// The umber of items
     ///
-    /// #Errors
+    /// # Errors
     /// Returns an error if the operation was not successful
-    fn copy(
-        from: Self::PathType,
-        to: Self::PathType,
-        check_timestamp: bool,
-        num_threads: i32,
-    ) -> Result<u32, Self::Error>;
+    fn perform_copy(threads: u32) -> Result<u32, Self::Error>;
 
-    /// Synchronizes the item and its destination instead of copying.
-    /// Eg. if one item exists in the destination or origin it is copied
-    /// to the other.
+    /// Prepares the target
     ///
-    /// #Parameters
+    /// # Parameters
+    /// - target: The config target to be prepared
     ///
-    /// -from: The path of the item
-    /// -to: The path of the destination
-    /// -num_threads: The number of threads to use (none if 1 or 0)
-    ///
-    /// #Returns
-    /// The umber of items
-    ///
-    /// #Errors
-    /// Returns an error if the operation was not successful
-    fn sync(from: Self::PathType, to: Self::PathType, num_threads: i32)
-        -> Result<u32, Self::Error>;
+    /// # Errors
+    /// Returns an error if the preparation failed
+    fn prepare_target(target: crate::config::BackupTarget) -> Result<(), Self::Error>;
 }
--- src/local_copy.rs
+++ src/local_copy.rs
@@ -1,28 +1,17 @@
 use crate::copy_operation::CopyOperation;
 use std::io::Error;
-use std::path::PathBuf;
 
 #[allow(dead_code)]
 pub struct LocalCopy;
 
 impl CopyOperation for LocalCopy {
-    type PathType = PathBuf;
     type Error = Error;
 
-    fn copy(
-        _from: Self::PathType,
-        _to: Self::PathType,
-        _check_timestamp: bool,
-        _num_threads: i32,
-    ) -> Result<u32, Self::Error> {
-        unimplemented!();
+    fn prepare_target(_target: crate::config::BackupTarget) -> Result<(), Self::Error> {
+        unimplemented!()
     }
 
-    fn sync(
-        _from: Self::PathType,
-        _to: Self::PathType,
-        _num_threads: i32,
-    ) -> Result<u32, Self::Error> {
-        unimplemented!();
+    fn perform_copy(_threads: u32) -> Result<u32, Self::Error> {
+        unimplemented!()
     }
 }
